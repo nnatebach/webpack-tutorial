845f39ba2042a518e70d0eb5c179940c515f30b0
Fastcoding_Nathan <minhnhan.fastcodingvn@gmail.com>
2023-10-23 13:25:05 +0700

    [add] 9. Introduction to Asset Modules

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,10 @@
-// Fix absolute path for "dist"
 const path = require('path')
 
 module.exports = {
-  // Entrypoint
   entry: './src/index.js',
-  // Output
   output: {
-    filename: 'bundle.js', // the output file => the result of the build process
-    path: path.resolve(__dirname, './dist') // the directory of the output file
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, './dist')
   },
-  // The last mandatory option
   mode: 'none'
 }
