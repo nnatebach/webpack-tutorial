d2be279395a9880469d94b7cf6934428928e999a
Fastcoding_Nathan <minhnhan.fastcodingvn@gmail.com>
2023-10-23 15:12:10 +0700

    [fix] 10. Handling Images With Webpack - clear comments

--- src/add-image.js
+++ src/add-image.js
@@ -1,17 +1,17 @@
-// 1. we want to use the image "kiwi.jpg" in this file => import the image
+// 1.
 import Kiwi from './kiwi.jpg'
 
 function addImage () {
-  // 2. There is no "img" element in the DOM just yet => create the element
+  // 2.
   const img = document.createElement('img')
-  // 3. Add image properties for the element "img"
+  // 3.
   img.alt = 'kiwi';
   img.width = 300;
   img.src = Kiwi;
-  // 4. The "img" has been created but NOT YET to be assigned in the DOM
+  // 4.
   const body = document.querySelector('body');
   body.appendChild(img);
 }
 
-// 5. Export the function => So we can use it in a different module
+// 5.
 export default addImage;
--- webpack.config.js
+++ webpack.config.js
@@ -7,20 +7,12 @@ module.exports = {
     path: path.resolve(__dirname, './dist')
   },
   mode: 'none',
-  // import image file
+
   module: {
     rules: [
-      // By default
-      // Webpack can understand JavaScript and JSON and can import them WITHOUT any additional rules
-      // Image files such as JPG are files that Webpack does not understand => additional rules NEEDED!
       {
-        // there are at least TWO properties for each rule.
-        // If the file passes the 'test' then Webpack will import the file by applying the 'type' we specify for that file
-
-        // 1.
-        test: /\.(png|jpg)$/, // checking whether the file name contains either PNG or JPG files
-        // 2. This property can be either 'type' or 'use'
-        type: 'asset/resource' // this type instructs Webpack to copy the required file into the output directory
+        test: /\.(png|jpg)$/,
+        type: 'asset/resource'
       }
     ]
   }
