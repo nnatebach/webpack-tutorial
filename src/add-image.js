fc363fc03715dc57216d00b8de7b1c7e03908706
Fastcoding_Nathan <minhnhan.fastcodingvn@gmail.com>
2023-10-24 11:43:44 +0700

    [add] 14. Asset/source Module Type

--- src/add-image.js
+++ src/add-image.js
@@ -1,11 +1,12 @@
 // 1.
 import Kiwi from './kiwi.jpg'
+import altText from './altText.txt'
 
 function addImage () {
   // 2.
   const img = document.createElement('img')
   // 3.
-  img.alt = 'kiwi';
+  img.alt = altText;
   img.width = 300;
   img.src = Kiwi;
   // 4.
--- webpack.config.js
+++ webpack.config.js
@@ -14,33 +14,18 @@ module.exports = {
     rules: [
       {
         test: /\.(png|jpg)$/,
-        type: 'asset', // asset bundle.js 4.27 KiB [emitted] => 'bundle.js' is much smaller compared to 2.65 MiB for "asset/inline"
-        // (general) asset
-        //// - Webpack will automatically choose between either asset/resource or asset/inline based on the size of each file.
-        //// if (size < 8 kilobytes) {
-        ////    return asset/inline
-        ////   } else {
-        ////      return asset/resource
-        ////   }
-        //// - Since the 'kiwi' image comes in big size => asset/resource => a separate file for our image in the dist directory is generated
-        //// - By checking the image in the developer's tool, we can see that the 'src' attribute containing the URL to the image file comes from inside the dist folder ('dist/23de234a71129d9c860b.jpg')
-
-
-        // Changing the preset '8 kilobytes' to something else.
-        // Read more:
-        //// https://webpack.js.org/configuration/module/#ruleparserdataurlcondition
-        //// https://webpack.js.org/guides/asset-modules/#general-asset-type
+        type: 'asset',
         parser: {
-          dataUrlCondition: { // the condition based on which Webpack decides if it should use asset/inline or asset/resource.
+          dataUrlCondition: {
             maxSize: 3 * 1024
-            // - now we've set the maximum file size to be 3 kilobytes from the default 8 kilobytes
-            // if (size < 3 kilobytes) {
-            //   return asset/inline
-            // } else {
-            //   return asset/resource
-            // }
             }
           }
+      },
+      {
+        // this rule targets text file
+        // we're telling Webpack to treat the text file we import as 'asset/source' => Webpack will give us a JavaScript string with the contents from the text file.
+        test: /\.txt/,
+        type: 'asset/source'
       }
     ]
   }
