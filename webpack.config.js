5793efda47c49fa9537f135f89a3379d27b8672c
Fastcoding_Nathan <minhnhan.fastcodingvn@gmail.com>
2023-10-25 11:28:13 +0700

    [add] 21. Minification of the Resulting Webpack Bundle

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,8 @@
 const path = require('path')
 
+// Terser - https://webpack.js.org/plugins/terser-webpack-plugin/
+const TerserPlugin = require('terser-webpack-plugin')
+
 module.exports = {
   entry: './src/index.js',
   output: {
@@ -26,7 +29,7 @@ module.exports = {
         test: /\.txt/,
         type: 'asset/source'
       },
-      // Rule for CSS - Read more: https://webpack.js.org/loaders/#styling
+      // Rule for styling (CSS) - Read more: https://webpack.js.org/loaders/#styling
       {
         // teach Webpack to read CSS file
         test: /\.css$/,
@@ -41,9 +44,13 @@ module.exports = {
           // Processes from Right to Left
           'style-loader', 'css-loader', 'sass-loader'
         ]
-      }
+      },
     ]
-  }
+  },
+  // Minification of the Resulting Webpack Bundle => asset bundle.js 4.88 KiB [emitted] [minimized] in comparison to 19.2 KiB (without minification)
+  plugins: [
+    new TerserPlugin()
+  ]
 }
 
 //// NOTE: The advantage of using asset modules (e.g. asset/source) is that we do not need to install any additional NPM packages.
