19883b38ef6e23c6276b70b82cde214897956181
Fastcoding_Nathan <minhnhan.fastcodingvn@gmail.com>
2023-10-26 11:36:32 +0700

    [add] 23. Extracting CSS Into a Separate Bundle With mini-css-extract-plugin, Part 2

--- src/index.js
+++ src/index.js
@@ -1,4 +1,15 @@
 import HelloWorldButton from './components/hello-world-button/hello-world.js'
+import Heading from './components/heading/heading.js'
 
 const helloWorldButton = new HelloWorldButton()
 helloWorldButton.render();
+
+// const heading = new heading()
+// ERROR: Module parse failed: Identifier 'heading' has already been declared
+// Solution: the name of the variable and the name of the 'new' Constructor MUST be different!!
+// NOTE: The name of the 'new' Constructor MUST be the SAME as the name used in 'import'
+const heading = new Heading()
+heading.render()
+
+//// NOTICE: 'helloWorldButton' button comes BEFORE the 'heading'
+// WHY?? Because we are rendering 'helloWorldButton' button BEFORE the 'heading'
