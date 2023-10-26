925082873e8e7453340b8e5a0656ea9bdd2b242b
Fastcoding_Nathan <minhnhan.fastcodingvn@gmail.com>
2023-10-26 11:47:51 +0700

    [clean] unnecessary comments

--- src/components/hello-world-button/hello-world.js
+++ src/components/hello-world-button/hello-world.js
@@ -1,11 +1,3 @@
-// using Babel in JavaScript
-// Reason: We want to use the latest JavaScript features in all browsers.
-// Problem: Not all browsers support the latest version of JavaScript => We might have to wait for some times in order to use those features.
-// Solution: Babel (JavaScript compiler) can convert modern JavaScript code into older JavaScript code that is already supported by all major browsers.
-//// NOTE: This problem is NO longer the case for the current JS and Chrome Version 118.0.5993.88 (Official Build) (arm64)
-// This lesson 18 'Using Latest JavaScript Features With Babel' can be used as a sample
-
-
 // import CSS file
 import './hello-world-button.scss'
 
--- webpack.config.js
+++ webpack.config.js
@@ -37,9 +37,7 @@ module.exports = {
         // teach Webpack to read CSS file
         test: /\.css$/,
         use: [
-          // 'style-loader', 'css-loader'
-
-          // modify code to use 'mini-css-extract-plugin'
+          // 'mini-css-extract-plugin'
           MiniCssExtractPlugin.loader, 'css-loader'
         ]
       },
@@ -47,26 +45,17 @@ module.exports = {
         // teach Webpack to read SCSS file
         test: /\.scss$/,
         use: [
-          // Processes from Right to Left
-          // 'style-loader', 'css-loader', 'sass-loader'
-
-          // modify code to use 'mini-css-extract-plugin'
+          // 'mini-css-extract-plugin'
           MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'
         ]
       },
     ]
   },
   plugins: [
-    // Minification of the Resulting Webpack Bundle => asset bundle.js 4.88 KiB [emitted] [minimized] in comparison to 19.2 KiB (without minification)
+    // Minification of the Resulting Webpack Bundle
     new TerserPlugin(),
 
     // Extract CSS into a separate file
-    // AND we can specify the file name if we want
-    // It creates a CSS file per JS file which contains CSS.
-    // asset bundle.js 953 bytes [emitted] [minimized] (name: main)
-    // asset style.css 176 bytes [emitted] (name: main)
-    // Entrypoint main 1.1 KiB = style.css 176 bytes bundle.js 953 bytes
-    // orphan modules 2.91 KiB (javascript) 937 bytes (runtime) [orphan] 7 modules
     new MiniCssExtractPlugin({
       filename: 'style.css'
     })
