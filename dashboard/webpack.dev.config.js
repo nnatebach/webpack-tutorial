bfacccd7f4e6b531e05979250b8a73328af5be8a
Fastcoding_Nathan <minhnhan.fastcodingvn@gmail.com>
2023-11-08 16:39:29 +0700

    [add] 56. Micro Frontends In Action, Part 1

---
+++ dashboard/webpack.dev.config.js
@@ -0,0 +1,52 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { ModuleFederationPlugin } = require('webpack').container;
+
+module.exports = {
+    entry: './src/dashboard.js',
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, './dist'),
+
+        // As we are exposing the page component to the application, we need to specify the publicPath where this application will be running.
+        publicPath: 'http://localhost:9002/'
+    },
+    mode: 'development',
+    devServer: {
+        contentBase: path.resolve(__dirname, './dist'),
+        index: 'dashboard.html',
+        port: 9000,
+        historyApiFallBack: {
+            index: 'dashboard.html'
+        }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [ '@babel/env' ],
+                    }
+                }
+            }
+        ]
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            filename: 'dashboard.html',
+            title: 'Dashboard',
+        }),
+        new ModuleFederationPlugin({
+            name: 'App',
+            remotes: {
+                HelloWorldApp: 'HelloWorldApp@http://localhost:9001/remoteEntry.js',
+                KiwiApp: 'KiwiApp@http://localhost:9002/remoteEntry.js'
+            }
+        })
+    ]
+};
