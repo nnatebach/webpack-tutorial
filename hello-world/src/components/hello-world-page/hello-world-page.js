dc3b50bc343b31dd2debf39e9413b50999d95834
Fastcoding_Nathan <minhnhan.fastcodingvn@gmail.com>
2023-11-10 13:39:00 +0700

    [fix] 56,57 Micro Frontends In Action - relative path

--- hello-world/src/components/hello-world-page/hello-world-page.js
+++ hello-world/src/components/hello-world-page/hello-world-page.js
@@ -1,5 +1,5 @@
-import HelloWorldButton from '../components/hello-world-button/hello-world-button.js';
-import Heading from '../components/heading/heading.js';
+import HelloWorldButton from '../hello-world-button/hello-world-button.js';
+import Heading from '../heading/heading.js';
 
 class HelloWorldPage {
   render() {
